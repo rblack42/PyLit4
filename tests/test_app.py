8e5b2fe12fce53ddb3dd2ce3221a2e0cad5c5297
Roie Black <rblack@austincc.edu>
2014-08-16 19:32:08 -0500

    fixed test_app errors

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ import unittest
 from pylit.app import create_app
 
 DIR_NAME = os.path.abspath(os.path.dirname(__file__))
-LOG_DIR = os.path.abspath(os.path.join(LOG_DIR,'../logs'))
+LOG_DIR = os.path.abspath(os.path.join(DIR_NAME,'../logs'))
 
 class Test_app(unittest.TestCase):
 
@@ -21,9 +21,10 @@ class Test_app(unittest.TestCase):
 
     def test_logging_file(self):
         resp = self.app.get('/')
+        self.app.logger.info("Test run")
         try:
             fout = open('debug','w')
-            log_filename = os.path.join(LOG_DIR, 'pylit.log'
+            log_filename = os.path.join(LOG_DIR, 'pylit.log')
             logfile = open(log_filename)
             isopen = True
         except:
